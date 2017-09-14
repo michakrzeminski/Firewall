6a506baaed76a51e45537f460b1dbcec76aa1d3f
michakrzeminski <michakrzeminski@gmail.com>
2017-09-14 13:44:20 +0000

    Switch packet buffer and analysis

--- Switch/Switch.cpp
+++ Switch/Switch.cpp
@@ -24,6 +24,8 @@ void Switch::init() {
     std::thread(&Client::read, client).detach();
     Protocol hello(HELLO);
     client->send(hello);
+
+    std::thread(&Switch::bufferHandling, this).detach();
 }
 
 void Switch::sniff() {
@@ -33,21 +35,41 @@ void Switch::sniff() {
 
 bool Switch::callback(Tins::PDU &pdu) {
     const Tins::IP &ip = pdu.rfind_pdu<Tins::IP>();
-    std::cout<< "from "<<ip.src_addr() <<" to "<<ip.dst_addr()<<std::endl;
+    std::cout<< "from "<<ip.src_addr() <<" to "<<ip.dst_addr()<<" protocol "<<ip.protocol()<<std::endl;
 	//miec jakos zebrana pule adresow czy cos
 	//Tins::Ipv4Address lo("127.0.0.1");
 	//Tins::HWAddress<6> hw_addr("01:de:22:01:09:af");
 	//i porownywac to co dostajemy moze z pula zapisana
-try {
-    std::string packet = ip.src_addr().to_string();
-    Protocol toCheck(CHECK);
-    toCheck.payload = packet;
-    Client::getInstance()->send(toCheck);
-}
-catch(std::exception &e) {
-    std::cout<<e.what();
-}
+    if(!analyzePacket(ip)) {
+        //do buffora
+        if(packetBuffer.size() <= NUM_PACKETS) {
+            packetBuffer.push_back(ip);
+        }
+    }
     return true;
-	//zeby zakonczyc sniffowanie
-	//return false
+    //zeby zakonczyc sniffowanie
+    //return false
+}
+
+bool Switch::analyzePacket(const Tins::IP &ip) {
+    for(auto i : packetMap) {
+        if(i.ip.src_addr() == ip.src_addr() && i.ip.dst_addr() == ip.dst_addr() && i.ip.protocol() == ip.protocol())
+            return true;
+    }
+    return false;
+}
+
+void Switch::bufferHandling() {
+    if(packetBuffer.size() != 0 ) {
+        auto tempPacket = packetBuffer.front();
+        packetBuffer.pop_front();
+        try {
+            Protocol toCheck(CHECK);
+            toCheck.payload = tempPacket;
+            Client::getInstance()->send(toCheck);
+        }
+        catch(std::exception &e) {
+            std::cout<<e.what();
+        }
+    }
 }
--- Switch/Switch.h
+++ Switch/Switch.h
@@ -3,16 +3,27 @@
 #include "Client.h"
 #include <thread>
 #include <tins/tins.h>
+#include <deque>
+#include <vector>
+
+struct PacketMap{
+    Tins::IP &ip;
+    Decision dec;
+};
 
 class Switch {
 public:
     static Switch* getInstance();
     void init();
     void sniff();
+    void bufferHandling();
 private:
     Switch();
     static Switch* instance;
     Tins::Sniffer* sniffer;
     bool callback(Tins::PDU &pdu);
+    bool analyzePacket(const Tins::IP &ip);
+    std::deque<Tins::IP> packetBuffer;
+    std::vector<PacketMap> packetMap;
 };
 #endif
